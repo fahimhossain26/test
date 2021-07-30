63b26f567eae3cf7c2ac04d0f8c130beec7697cf
fahimhossian26 <ismailhossainfahim64@gmail.com>
2021-07-31 00:09:14 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,13 @@
+var a='I am'
+var b='fahim hosssain'
+
+var c = a.concat(' ',b)
+ console.log(c)
+var d=c.substr(11,6)
+console.log(d)
+console.log(c.charAt(5))
+console.log(c.startsWith('i'))
+console.log(a.toUpperCase())
+console.log(a.toLocaleLowerCase())
+console.log('    asdffggdddsg   '.trim())
+console.log(c.split(' '))
